a3d5266feddf673a5ba7e9436ec3910c4c26ac12
Tommaso <tommaso.macco@gmail.com>
2021-11-09 16:51:52 +0100

    first push

---
+++ script.js
@@ -0,0 +1,19 @@
+// Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nostro javascript in cui:
+// - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. (nel nostro file js)
+// Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+// Consigli del giorno:
+// Leggiamo bene le specifiche, comprendiamole e studiamo bene ciò che abbiamo di partenza (in questo caso abbiamo HTML e CSS già pronti e da non modificare)
+// Ragioniamo come sempre a step.
+// Prima la logica in italiano e poi traduciamo in codice. console.log() è nostro amico.
+// Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
+// Risolviamo solo una cosa alla volta cercando di testare e tenere la soluzione più semplice possibile prima di procedere al prossimo step.
+
+
+let teamName = [""]
+
+document.getElementById("addMemberButton").addEventListener("click",
+let nome = document.getElementById("name").value
+console.log(nome)
+)
